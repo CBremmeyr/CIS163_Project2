f1d1fbff1755f5d7362b40f27972f39508df7901
Corbin Bremmeyr <corbinbremmeyr@gmail.com>
2019-01-28 18:10:44 -0500

    Started Cell class

--- src/Project2/Cell.java
+++ src/Project2/Cell.java
@@ -3,4 +3,65 @@
 package Project2;
 
 public class Cell {
+
+
+    private int mineCount;
+
+
+    private boolean isFlagged;
+
+
+    private boolean isExposed;
+
+
+    private boolean isMine;
+
+
+    public Cell() {
+
+        this.mineCount = 0;
+        this.isFlagged = false;
+        this.isExposed = false;
+        this.isMine = false;
+    }
+
+
+    public int getMineCount() {
+        return mineCount;
+    }
+
+
+    public void setMineCount(int mineCount) {
+        this.mineCount = mineCount;
+    }
+
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+
+    public void setFlagged(boolean flagged) {
+        isFlagged = flagged;
+    }
+
+
+    public boolean isExposed() {
+        return isExposed;
+    }
+
+
+    public void setExposed(boolean exposed) {
+        isExposed = exposed;
+    }
+
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
 }
