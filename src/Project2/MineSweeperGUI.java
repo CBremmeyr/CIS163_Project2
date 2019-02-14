541719ba724123c1e3790bcb4b25b685ed895876
Michael James <jamesmic@mail.gvsu.edu>
2019-02-14 12:52:38 -0500

    Started GUI classes

--- src/Project2/MineSweeperGUI.java
+++ src/Project2/MineSweeperGUI.java
@@ -1,4 +1,21 @@
 package Project2;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class MineSweeperGUI {
+public static void main(String arg[]){
+    JFrame gui = new JFrame();
+
+    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    gui.setTitle("Mine Sweeper (May the clicks be with you");
+
+    gui.setContentPane(new MineSweeperPanel());
+    gui.setSize(400,400)
+    gui.setVisibile(true);
+
+
+
+
+
 }
--- src/Project2/MineSweeperPanel.java
+++ src/Project2/MineSweeperPanel.java
@@ -1,4 +1,88 @@
 package Project2;
 
-public class MineSweeperPanel {
+public class MineSweeperPanel extends JPanel implements ActionListener{
+
+
+    private JButton[][] board;
+    private JButton quitButton;
+    private JButton resetButton;
+    private Cell iCell;
+    private JButton quitButton;
+    private MineSweeperGame game;
+
+
+MineSweeperPanel (){
+    game = new MineSweeperGame();
+    JPanel panel = new JPanel;
+
+
+    for(int row = 0; row< 10; row++)
+    {
+        for (int col = 0; col < 10; col++) {
+            board[row][col] = new JButton("", emptyIcon);
+            board[row][col].addActionListener(listener);
+            panel.add(board[row][col]);
+        }
+    }
+    emptyIcon = new ImageIcon(); //add on to this with book
+
+
+
+    }
+
+
+
+    private displayBoard() {
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                iCell = game.getCell(row, col);
+                if (iCell.isExposed())
+                    board[row][col].setEnabled(false);
+                else
+                    board[row][col].setEnabled(true);
+                if (iCell.isExposed() && getMineCount() > 0)
+                    board[row][col].setText("" + getMineCount());
+                else {
+                    board[row][col].setText(" ");
+                }
+            }
+
+        }
+    }
+
+
+    private class Buttonistener implements ActionListener(){
+
+    private actionPreformed() {
+            for (int row = 0; row < 10; row++) {
+                for (int col = 0; col < 10; col++) {
+                    if (board[row][col] == e.getSource())
+                        game.select(row, col);
+                }
+            }
+            if (quitButton == e.getSource()){
+
+            }
+            if (resetButton == e.getSource()){
+                game.reset();
+            }
+        displayboard();
+
+        if (game.getGameStatus() == GameStatus.Lost) {
+            JOptionPane.showMessageDialog(null, "You Lose”);
+        }
+        if (game.getGameStatus() == GameStatus.Won){
+            JOptionPane.showMessageDialog(null, "You Win”);
+        }
+
+    }
+    }
+
+
+
+
 }
+
+
+
+
