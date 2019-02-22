41161205a216ed68ac85bca4a17179d6ca603e10
Corbin Bremmeyr <corbinbremmeyr@gmail.com>
2019-02-22 13:14:29 -0500

    Got GUI window to open

--- src/Project2/MineSweeperGUI.java
+++ src/Project2/MineSweeperGUI.java
@@ -4,18 +4,18 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
 
-public static void main(String arg[]){
-    JFrame gui = new JFrame();
 
-    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui.setTitle("Mine Sweeper (May the clicks be with you");
+class MineSweeperGUI {
 
-    gui.setContentPane(new MineSweeperPanel());
-    gui.setSize(400,400)
-    gui.setVisibile(true);
+    public static void main(String arg[]) {
+        JFrame gui = new JFrame();
 
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setTitle("Mine Sweeper (May the clicks be with you");
 
+        gui.setContentPane(new MineSweeperPanel());
+        gui.setSize(400, 400);
+        gui.setVisible(true);
 
-
-
+    }
 }
