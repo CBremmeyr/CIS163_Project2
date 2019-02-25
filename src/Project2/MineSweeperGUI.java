1f3d6685e763925ec0f9a4189e875ebb4a00260f
Corbin Bremmeyr <corbinbremmeyr@gmail.com>
2019-02-25 17:03:45 -0500

    Gets size and mine count from user with JOptionPane

--- src/Project2/MineSweeperGUI.java
+++ src/Project2/MineSweeperGUI.java
@@ -10,10 +10,19 @@ public class MineSweeperGUI {
     public static void main(String arg[]) {
 
         JFrame gui = new JFrame();
-        int boardSize = 10;
-        int mineCount = 10;
+        String boardSizeInput;
+        String mineInput;
+        int boardSize;
+        int mineCount;
 
         // TODO: Get board size and mine count from user with jOptionPane
+        boardSizeInput = JOptionPane.showInputDialog("Enter Board Size");
+        mineInput = JOptionPane.showInputDialog("Enter Number of Mines");
+
+        boardSize = Integer.parseInt(boardSizeInput);
+        mineCount = Integer.parseInt(mineInput);
+
+        // TODO: Check for valid board size and mine count
 
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gui.setTitle("Mine Sweeper (May the clicks be with you");
