ba48e2c51c3a58f439ad2d07800ee0b012413a96
Corbin Bremmeyr <corbinbremmeyr@gmail.com>
2019-02-25 16:43:51 -0500

    Added javadoc comment

--- src/Project2/GameStatus.java
+++ src/Project2/GameStatus.java
@@ -1,6 +1,12 @@
 package Project2;
 
-// TODO: add javadoc comment
+/**********************************************************************
+ * Enum to track state of the game.
+ *
+ * @author Corbin Bremmeyr
+ * @author Michael James
+ * @version 25 February 2019
+ *********************************************************************/
 public enum GameStatus {
     Lost,
     Won,
